717f1c67393dacadb430bf0445c0ba7d0fbb9eff
yarslivje <yarslivje@gmail.com>
2025-10-23 10:49:35 +0300

    Labaratory7

---
+++ project7/project7.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	double y, x, h, minX, maxX;
+	cout << "Enter [minX, maxX], h ";
+	cin >> minX >> maxX >> h;
+	cout << "x -> [" << minX << " , " << maxX << "] , h = " << h << endl;
+	cout << "\tWHILE\n" << setw(8) << "x|" << setw(7) << "y" << endl;
+	cout << setw(16) << setfill('-') << '\n' << setfill(' ');
+	x = minX;
+	while (x <= maxX) {
+		 y = sin(x) + pow(x, 2);
+		cout << fixed << setprecision(2) << setw(7) << x << '|' << setw(7) << y << endl;
+		x += h;
+
+	}
+	cout << "\n\tDO_WHILE\n" << setw(8) << "x|" << setw(7) << "y" << endl;
+	cout << setw(16) << setfill(' - ') << '\n' << setfill(' ');
+	x = minX;
+	do {
+		y = sin(x) + pow(x, 2);
+		cout << fixed << setprecision(2) << setw(7) << x << '|' << setw(7) << y << endl;
+		x += h;
+	} 
+	while (x <= maxX);
+}
